375e5150926dac55032d294f3d4dbeaf987747d7
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-11-13 13:20:29 -0500

    first commit--need to figure out how to track beginnings

---
+++ three_in_one.py
@@ -0,0 +1,39 @@
+class triple_stack(list):
+    def __init__(self, top1 = 0, top2 = 0, top3 = 0):
+        self.marker1 = 0
+        self.marker2 = 1
+        self.marker3 = 2
+        self = [top1, top2, top3]
+
+
+    def id_marker(self, marker):
+        mark = None
+        if stack == 0:
+            mark = (self.marker1, self.marker2, self.marker3)
+        elif stack == 1:
+            mark = (self.marker2, self.marker1, self.marker3)
+        elif stack == 2:
+            mark = (self.marker3, self.marker1, self.marker2)
+
+        return mark
+
+
+    def push(self, item, stack):
+        mark, oth1, oth2 = id_marker(stack)    
+        self = self[:mark-1] + item + self[mark-1:]
+
+
+    def pop(self, stack):
+        mark, oth1, oth2 = id_marker(stack)
+        self = self[:mark-1] + self[mark:]
+        return self[mark]
+
+
+    
+
+if __name__ == '__main__':
+    test_triple_stack = triple_stack()
+    test_triple_stack.push(1, 0)
+    test_triple_stack.push(5, 1)
+    test_triple_stack.push(9, 2)
+    print(5==test_triple_stack.pop(1))
