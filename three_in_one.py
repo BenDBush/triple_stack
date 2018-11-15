6933ed47444e30b52b279840ef2273cc56046f73
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-11-15 14:58:39 -0500

    Appears to work for a small triple stack

--- three_in_one.py
+++ three_in_one.py
@@ -1,42 +1,43 @@
-class triple_stack(list):
+class triple_stack:
     def __init__(self, top1 = 0, top2 = 0, top3 = 0):
-        self.marker1 = 0
-        self.marker2 = 1
-        self.marker3 = 2
-        self = [top1, top2, top3]
-        print(self)
+        mark1, mark2, mark3 = (0,1,2)
+        self.markers = [mark1,mark2,mark3]
+        self.list = [top1, top2, top3]
 
-
-    def id_marker(self, stack):
-        mark = None
-        if stack == 0:
-            mark = (self.marker1, self.marker2, self.marker3)
-        elif stack == 1:
-            mark = (self.marker2, self.marker1, self.marker3)
-        elif stack == 2:
-            mark = (self.marker3, self.marker1, self.marker2)
-
-        return mark
 
 
     def push(self, item, stack):
-        mark, oth1, oth2 = self.id_marker(stack)    
-        self = self[:mark-1] + [item] + self[mark-1:]
+        mark = self.markers[stack]    
+        self.list = self.list[:mark] + [item] + self.list[mark:]
+        for i in range(len(self.markers)):
+            if self.markers[i] > mark:
+                self.markers[i] += 1
+
+
 
 
     def pop(self, stack):
-        mark, oth1, oth2 = self.id_marker(stack)
-        popped_item = self[mark]
-        self = self[:mark-1] + self[mark:]
+        mark = self.markers[stack]
+        popped_item = self.list[mark]
+        self.list = self.list[:mark] + self.list[mark+1:]
+        for i in range(len(self.markers)):
+            if self.markers[i] > mark:
+                self.markers[i] -= 1
+
+
         return popped_item
 
 
     
 
 if __name__ == '__main__':
-    test_triple_stack = triple_stack()
-    test_triple_stack.push(1, 0)
+    test_triple_stack = triple_stack(top1=1, top2 = 2, top3 = 3)
+    print(test_triple_stack.list)
+    test_triple_stack.push(13, 0)
     test_triple_stack.push(5, 1)
     test_triple_stack.push(9, 2)
-    print(test_triple_stack)
-    print(5==test_triple_stack.pop(1))
+    print(test_triple_stack.list)
+    print(test_triple_stack.markers)
+    print(test_triple_stack.pop(1))
+    print(test_triple_stack.list)
+    print(test_triple_stack.markers)
